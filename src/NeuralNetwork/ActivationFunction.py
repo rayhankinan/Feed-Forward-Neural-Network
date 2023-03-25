3b71e1832ff637c0b3aca76f4d04d87f3e2918da
rayhankinan <rayhankinan@gmail.com>
2023-03-26 00:02:32 +0700

    Add softmax activation function

--- src/NeuralNetwork/ActivationFunction.py
+++ src/NeuralNetwork/ActivationFunction.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-
 import numpy as np
 from typing import NamedTuple
 
@@ -18,7 +17,8 @@ class ActivationFunction(NamedTuple):
 class LinearActivationFunction(ActivationFunction):
     def __new__(cls) -> LinearActivationFunction:
         self = super(LinearActivationFunction, cls).__new__(
-            cls, np.vectorize(lambda x: x),
+            cls,
+            np.vectorize(lambda x: x),
             np.vectorize(lambda _: 1),
         )
 
@@ -41,9 +41,30 @@ class SigmoidActivationFunction(ActivationFunction):
         self = super(SigmoidActivationFunction, cls).__new__(
             cls,
             np.vectorize(lambda x: 1 / (1 + np.exp(-x))),
-            np.vectorize(lambda x: x * (1 - x)),
+            np.vectorize(lambda x: np.exp(-x) / (1 + np.exp(-x)) ** 2),
         )
 
         return self
 
-# TODO: Menambahkan fungsi aktivasi SoftMax
+
+class SoftmaxActivationFunction(ActivationFunction):
+    def __new__(cls) -> SoftmaxActivationFunction:
+
+        # Inner Function
+        def softmax(x: np.ndarray) -> np.ndarray:
+            shift_x = np.array(x - np.max(x))
+            exps = np.exp(shift_x)
+            return np.array(exps / np.sum(exps, axis=0))
+
+        # Inner Function
+        def derivative_softmax(x: np.ndarray) -> np.ndarray:
+            s = softmax(x).reshape(-1, 1)
+            return np.array(np.diagflat(s) - np.dot(s, s.T))
+
+        self = super(SoftmaxActivationFunction, cls).__new__(
+            cls,
+            np.vectorize(softmax),
+            np.vectorize(derivative_softmax),
+        )
+
+        return self
