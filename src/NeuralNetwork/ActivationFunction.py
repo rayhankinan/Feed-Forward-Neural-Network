Fix softmax function

src/NeuralNetwork/ActivationFunction.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 import numpy as np
-from typing import NamedTuple
+from typing import NamedTuple, Union, Callable
 
 
 class ActivationFunction(NamedTuple):
-    function: np.vectorize
-    derivative: np.vectorize
+    function: Union[np.vectorize, Callable[[np.ndarray], np.ndarray]]
+    derivative: Union[np.vectorize, Callable[[np.ndarray], np.ndarray]]
 
     def __call__(self, x) -> np.ndarray:
         return np.array(self.function(x))
@@ -63,8 +63,8 @@ class SoftmaxActivationFunction(ActivationFunction):
 
         self = super(SoftmaxActivationFunction, cls).__new__(
             cls,
-            np.vectorize(softmax),
-            np.vectorize(derivative_softmax),
+            softmax,
+            derivative_softmax,
         )
 
         return self

src/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from NeuralNetwork import Perceptron, Layer, SigmoidActivationFunction, NeuralNetwork
+from NeuralNetwork import Perceptron, Layer, SoftmaxActivationFunction, NeuralNetwork
 
 
 if __name__ == "__main__":
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                     Perceptron(weight=np.array([0.1, 0.2, 0.3])),
                     Perceptron(weight=np.array([0.4, 0.5, 0.6])),
                 ],
-                activation_function=SigmoidActivationFunction(),
+                activation_function=SoftmaxActivationFunction(),
             )
         ]
     )