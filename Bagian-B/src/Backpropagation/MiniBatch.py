Changes to backprop (still wrong value)

Bagian-B/src/Backpropagation/MiniBatch.py
@@ -13,34 +13,19 @@ class MiniBatch(NamedTuple):
             self.partitioned_learning_data
         )
         result: NeuralNetwork = self.neural_network
-        previous_delta_error: np.ndarray = np.array([])
+        previous_delta_error: np.ndarray = None
 
-        for i in range(len(list_of_output) - 1, -1):
+        for i in range(len(list_of_output) - 1, -1, -1):
+            raw_x = self.partitioned_learning_data if i == 0 else list_of_output[i - 1]
+            x = np.array(np.c_[np.ones(raw_x.shape[0]), raw_x])
+
             o = list_of_output[i]
             t = self.partitioned_learning_target[i]
             derivated_output = result.list_of_layer[i].activation_function.get_derivative_output(
                 o
             )
 
-            if i == 0:
-                x = self.partitioned_learning_data
-                output_weight = result.list_of_layer[i + 1].get_weight()
-                delta_error = np.multiply(
-                    np.dot(
-                        previous_delta_error, output_weight.T
-                    ),
-                    derivated_output
-                )
-                delta_weight = np.array(
-                    np.dot(learning_rate, np.dot(x.T, delta_error))
-                )
-                result.list_of_layer[i] = result.list_of_layer[i].get_updated_weight(
-                    delta_weight
-                )
-                previous_delta_error = delta_error
-
-            elif i == len(list_of_output) - 1:
-                x = list_of_output[i - 1]
+            if i == len(list_of_output) - 1:
                 delta_error = np.multiply(np.subtract(t, o), derivated_output)
                 delta_weight = np.array(
                     np.dot(learning_rate, np.dot(x.T, delta_error))
@@ -51,7 +36,6 @@ class MiniBatch(NamedTuple):
                 previous_delta_error = delta_error
 
             else:
-                x = list_of_output[i - 1]
                 output_weight = result.list_of_layer[i + 1].get_weight()
                 delta_error = np.multiply(
                     np.dot(
@@ -66,7 +50,5 @@ class MiniBatch(NamedTuple):
                     delta_weight
                 )
                 previous_delta_error = delta_error
-
-        # TODO: Update Weight
 
         return result

Bagian-B/src/FileAccess/FileSystem.py
@@ -47,21 +47,21 @@ class FileSystem:
             initial_neural_network = NeuralNetwork(list_of_layer)
 
             test_case_size = int(file.readline().rstrip('\n'))
-            input_array = np.array([])
-            target_array = np.array([])
+            input_array: list[np.ndarray] = []
+            target_array: list[np.ndarray] = []
 
             for _ in range(test_case_size):
                 input_vector = list(
                     map(float, file.readline().rstrip('\n').split())
                 )
 
-                input_array = np.append(input_array, input_vector)
+                input_array.append(np.array(input_vector))
 
             for _ in range(test_case_size):
                 target_vector = list(
                     map(float, file.readline().rstrip('\n').split())
                 )
-                target_array = np.append(target_array, target_vector)
+                target_array.append(np.array(target_vector))
 
             learning_rate = float(file.readline().rstrip('\n'))
             mini_batch_size = int(file.readline().rstrip('\n'))
@@ -69,7 +69,9 @@ class FileSystem:
             threshold = float(file.readline().rstrip('\n'))
 
             backpropagation = Backpropagation(
-                initial_neural_network, input_array, target_array
+                initial_neural_network,
+                np.array(input_array),
+                np.array(target_array)
             )
 
             return backpropagation.learn(learning_rate, mini_batch_size, max_iter, threshold)

Bagian-B/src/main.py
@@ -1,13 +1,11 @@
-import numpy as np
 from FileAccess import FileSystem
 
 
 if __name__ == "__main__":
     neural_network = FileSystem.read_neural_network(
-        "./Bagian-B/test-cases/test-case-1.txt"
+        "./Bagian-B/test-cases/linear.txt"
     )
 
-    output_x = neural_network.get_output(
-        np.array([1., 2.])
-    )
-    print(output_x)
+    for weight in neural_network.get_weight():
+        print(weight)
+        print()