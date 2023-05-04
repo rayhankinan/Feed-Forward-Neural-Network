b59a23a56ff6cf3f6a7b225636e0dcab1b08066d
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-05-04 18:45:58 +0700

    testing bagian B

--- Bagian-B/src/main.py
+++ Bagian-B/src/main.py
@@ -2,10 +2,13 @@ from FileAccess import FileSystem
 
 
 if __name__ == "__main__":
-    neural_network = FileSystem.read_neural_network(
-        "./Bagian-B/test-cases/txt/softmax.txt"
-    )
+    test_case = ["linear_small_lr", "linear_two_iteration", "linear", "mlp", "relu", "sigmoid_mini_batch_GD", "sigmoid_stochastic_GD", "sigmoid", "softmax_error_only", "softmax", "sse_only"]
+    for i in range(len(test_case)):
+        print(test_case[i])
+        neural_network = FileSystem.read_neural_network(
+            "./Bagian-B/test-cases/txt/" + test_case[i] + ".txt"
+        )
 
-    print("Weight:")
-    for weight in neural_network.get_weight():
-        print(weight)
+        for weight in neural_network.get_weight():
+            print(weight)
+            print()
