489b7bc5962833a5075a1fc80f2b90a462bffcf9
rayhankinan <rayhankinan@gmail.com>
2023-05-04 21:05:18 +0700

    Add save and load to file

--- Bagian-B/src/FileAccess/FileSystem.py
+++ Bagian-B/src/FileAccess/FileSystem.py
@@ -5,27 +5,111 @@ from Backpropagation import Backpropagation, ErrorFunction, SumOfSquaredErrorFun
 
 class FileSystem:
     @staticmethod
-    def read_neural_network(path) -> NeuralNetwork:
-        with open(path, 'r') as file:
-            input_size = int(file.readline().rstrip('\n'))
-            num_of_layers = int(file.readline().rstrip('\n'))
+    def load_from_file(path: str) -> NeuralNetwork:
+        with open(path, "r") as file:
+            input_size = int(file.readline().rstrip())
+            num_of_layers = int(file.readline().rstrip())
 
             prev_num_of_perceptrons = input_size
             list_of_layer: list[Layer] = []
             latest_activation_function_type: str = None
 
             for _ in range(num_of_layers):
-                num_of_perceptrons = int(file.readline().rstrip('\n'))
+                num_of_perceptrons = int(file.readline().rstrip())
                 list_of_weight_row: list[Row] = []
 
                 for _ in range(prev_num_of_perceptrons + 1):
                     weight = np.array(
-                        list(map(float, file.readline().rstrip('\n').split()))
+                        list(map(float, file.readline().rstrip().split()))
                     )
                     row = Row(weight)
                     list_of_weight_row.append(row)
 
-                activation_function_type = file.readline().rstrip('\n')
+                activation_function_type = file.readline().rstrip()
+
+                if latest_activation_function_type == "softmax":
+                    raise NotImplementedError()
+
+                activation_function: ActivationFunction
+                match activation_function_type:
+                    case "linear":
+                        activation_function = LinearActivationFunction()
+                    case "relu":
+                        activation_function = ReLUActivationFunction()
+                    case "sigmoid":
+                        activation_function = SigmoidActivationFunction()
+                    case "softmax":
+                        activation_function = SoftmaxActivationFunction()
+                    case _:
+                        raise NotImplementedError()
+
+                layer = Layer(list_of_weight_row, activation_function)
+                list_of_layer.append(layer)
+
+                prev_num_of_perceptrons = num_of_perceptrons
+                latest_activation_function_type = activation_function_type
+
+            neural_network = NeuralNetwork(list_of_layer)
+
+            return neural_network
+
+    @staticmethod
+    def save_to_file(neural_network: NeuralNetwork, path: str) -> None:
+        with open(path, "w") as file:
+            input_weight = neural_network.list_of_layer[0].get_weight()
+            input_size = input_weight.shape[0] - 1
+            file.write(f"{input_size}\n")
+
+            num_of_layers = len(neural_network.list_of_layer)
+            file.write(f"{num_of_layers}\n")
+
+            for layer in neural_network.list_of_layer:
+                weight = layer.get_weight()
+                num_of_perceptrons = weight.shape[1]
+                file.write(f"{num_of_perceptrons}\n")
+
+                for i in range(weight.shape[0]):
+                    for j in range(weight.shape[1]):
+                        file.write(f"{weight[i][j]} ")
+                    file.write("\n")
+
+                activation_function_type: str = None
+                match layer.activation_function:
+                    case LinearActivationFunction():
+                        activation_function_type = "linear"
+                    case ReLUActivationFunction():
+                        activation_function_type = "relu"
+                    case SigmoidActivationFunction():
+                        activation_function_type = "sigmoid"
+                    case SoftmaxActivationFunction():
+                        activation_function_type = "softmax"
+                    case _:
+                        raise NotImplementedError()
+
+                file.write(f"{activation_function_type}\n")
+
+    @staticmethod
+    def learn_from_file(path: str) -> NeuralNetwork:
+        with open(path, "r") as file:
+            input_size = int(file.readline().rstrip())
+            num_of_layers = int(file.readline().rstrip())
+
+            prev_num_of_perceptrons = input_size
+            list_of_layer: list[Layer] = []
+            latest_activation_function_type: str = None
+
+            for _ in range(num_of_layers):
+                num_of_perceptrons = int(file.readline().rstrip())
+                list_of_weight_row: list[Row] = []
+
+                for _ in range(prev_num_of_perceptrons + 1):
+                    weight = np.array(
+                        list(map(float, file.readline().rstrip().split()))
+                    )
+                    row = Row(weight)
+                    list_of_weight_row.append(row)
+
+                activation_function_type = file.readline().rstrip()
 
                 if latest_activation_function_type == "softmax":
                     raise NotImplementedError()
@@ -51,27 +135,27 @@ class FileSystem:
 
             initial_neural_network = NeuralNetwork(list_of_layer)
 
-            test_case_size = int(file.readline().rstrip('\n'))
+            test_case_size = int(file.readline().rstrip())
             input_array: list[np.ndarray] = []
             target_array: list[np.ndarray] = []
 
             for _ in range(test_case_size):
                 input_vector = list(
-                    map(float, file.readline().rstrip('\n').split())
+                    map(float, file.readline().rstrip().split())
                 )
 
                 input_array.append(np.array(input_vector))
 
             for _ in range(test_case_size):
                 target_vector = list(
-                    map(float, file.readline().rstrip('\n').split())
+                    map(float, file.readline().rstrip().split())
                 )
                 target_array.append(np.array(target_vector))
 
-            learning_rate = float(file.readline().rstrip('\n'))
-            mini_batch_size = int(file.readline().rstrip('\n'))
-            max_iter = int(file.readline().rstrip('\n'))
-            threshold = float(file.readline().rstrip('\n'))
+            learning_rate = float(file.readline().rstrip())
+            mini_batch_size = int(file.readline().rstrip())
+            max_iter = int(file.readline().rstrip())
+            threshold = float(file.readline().rstrip())
 
             backpropagation = Backpropagation(
                 initial_neural_network,
--- Bagian-B/src/main.py
+++ Bagian-B/src/main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     for i in range(len(test_case)):
         print(test_case[i])
-        neural_network = FileSystem.read_neural_network(
+        neural_network = FileSystem.learn_from_file(
             f"./Bagian-B/test-cases/txt/{test_case[i]}.txt"
         )
 
@@ -15,3 +15,15 @@ if __name__ == "__main__":
         for weight in neural_network.get_weight():
             print(weight)
         print()
+
+        FileSystem.save_to_file(
+            neural_network, f"./Bagian-B/model/{test_case[i]}.txt"
+        )
+
+    # new_neural_network = FileSystem.load_from_file(
+    #     "./Bagian-B/model/linear.txt")
+
+    # print("Weight:")
+    # for weight in new_neural_network.get_weight():
+    #     print(weight)
+    # print()
