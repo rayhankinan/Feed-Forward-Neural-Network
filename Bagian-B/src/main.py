56c8f7cd9925003c92df08e2372f69abe2a0dc08
rayhankinan <rayhankinan@gmail.com>
2023-05-05 03:47:30 +0700

    Add numpy iris data

--- Bagian-B/src/main.py
+++ Bagian-B/src/main.py
@@ -1,30 +1,21 @@
+import numpy as np
+from sklearn.datasets import load_iris
 from FileAccess import FileSystem
 
 
 if __name__ == "__main__":
-    test_case = ["linear_small_lr", "linear_two_iteration", "linear", "mlp", "relu", "sigmoid_mini_batch_GD",
-                 "sigmoid_stochastic_GD", "sigmoid", "softmax_error_only", "softmax", "sse_only"]
+    iris = load_iris()
 
-    for i in range(len(test_case)):
-        print(test_case[i])
-        neural_network = FileSystem.learn_from_file(
-            f"./Bagian-B/test-cases/txt/{test_case[i]}.txt"
-        )
+    learning = np.array(iris.data)
+    target = np.array(iris.target)
 
-        print("Weight:")
-        for weight in neural_network.get_weight():
-            print(weight)
-        print()
+    one_hot_target = np.zeros((target.size, target.max() + 1))
+    one_hot_target[np.arange(target.size), target] = 1
 
-        FileSystem.save_to_file(
-            neural_network, f"./Bagian-B/model/{test_case[i]}.txt"
-        )
+    neural_network = FileSystem.load_from_file(
+        "./Bagian-B/model/iris-raw.txt"
+    )
 
-    # new_neural_network = FileSystem.load_from_file(
-    #     "./Bagian-B/model/linear.txt"
-    # )
-
-    # print("Weight:")
-    # for weight in new_neural_network.get_weight():
-    #     print(weight)
-    # print()
+    print("Weight:")
+    for weight in neural_network.get_weight():
+        print(weight)
