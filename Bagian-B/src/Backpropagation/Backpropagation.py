f29eeca9c82596fef9e33182f85e6598e76faf64
rayhankinan <rayhankinan@gmail.com>
2023-05-04 14:42:07 +0700

    Minor changes to error

--- Bagian-B/src/Backpropagation/Backpropagation.py
+++ Bagian-B/src/Backpropagation/Backpropagation.py
@@ -15,7 +15,7 @@ class Backpropagation(NamedTuple):
         result: NeuralNetwork = self.neural_network
 
         for _ in range(max_iter):
-            if current_error < threshold:
+            if current_error <= threshold:
                 break
 
             start_index = 0
--- Bagian-B/src/main.py
+++ Bagian-B/src/main.py
@@ -3,7 +3,7 @@ from FileAccess import FileSystem
 
 if __name__ == "__main__":
     neural_network = FileSystem.read_neural_network(
-        "./Bagian-B/test-cases/txt/sigmoid.txt"
+        "./Bagian-B/test-cases/txt/linear_two_iteration.txt"
     )
 
     for weight in neural_network.get_weight():
