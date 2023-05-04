e57dc2e899ef3db4d78037a727a209d553b25c1a
rayhankinan <rayhankinan@gmail.com>
2023-05-04 17:11:17 +0700

    Fix sse again

--- Bagian-B/src/Backpropagation/Backpropagation.py
+++ Bagian-B/src/Backpropagation/Backpropagation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from typing import NamedTuple
 from NeuralNetwork import NeuralNetwork
 from . import MiniBatch, ErrorFunction
@@ -12,21 +13,17 @@ class Backpropagation(NamedTuple):
     def learn(self, learning_rate: float, mini_batch_size: int, max_iter: int, threshold: float, error_function: ErrorFunction) -> NeuralNetwork:
         data_length = self.learning_data.shape[0]
         result: NeuralNetwork = self.neural_network
+        current_error = np.inf
+        index = 0
 
-        for i in range(max_iter):
+        # Until the error is less than the threshold or the maximum iteration is reached
+        while current_error >= threshold and index < max_iter:
+            start_time = time.time()
+
+            # Get Output
             current_output = result.get_batch_output(self.learning_data)
-            current_error = error_function(
-                current_output,
-                self.learning_target
-            )
 
-            print(
-                f"Epoch ke-{i + 1} | Error: {current_error}"
-            )
-
-            if current_error <= threshold:
-                break
-
+            # Mini-batch learning
             start_index = 0
             while start_index < data_length:
                 end_index = min(start_index + mini_batch_size, data_length)
@@ -40,5 +37,19 @@ class Backpropagation(NamedTuple):
                 )
                 result = mini_batch.learn(learning_rate)
                 start_index += mini_batch_size
+
+            # Get Error
+            current_error = error_function(
+                current_output,
+                self.learning_target
+            )
+
+            finish_time = time.time()
+
+            print(
+                f"Epoch {index + 1} | Error: {current_error} | Time: {round(1000 * (finish_time - start_time), 2)} ms"
+            )
+
+            index += 1
 
         return result
--- Bagian-B/src/main.py
+++ Bagian-B/src/main.py
@@ -3,9 +3,9 @@ from FileAccess import FileSystem
 
 if __name__ == "__main__":
     neural_network = FileSystem.read_neural_network(
-        "./Bagian-B/test-cases/txt/mlp.txt"
+        "./Bagian-B/test-cases/txt/sse_only.txt"
     )
 
+    print("Weight:")
     for weight in neural_network.get_weight():
         print(weight)
-        print()
