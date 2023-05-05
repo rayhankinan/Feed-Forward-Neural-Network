32b6ebe58ccd06fdf6eb1a248cf8e7600bcc77e8
rayhankinan <rayhankinan@gmail.com>
2023-05-05 17:00:56 +0700

    Delete print delta weight

--- Bagian-B/src/Backpropagation/MiniBatch.py
+++ Bagian-B/src/Backpropagation/MiniBatch.py
@@ -36,7 +36,6 @@ class MiniBatch(NamedTuple):
                 delta_weight = np.array(
                     np.dot(learning_rate, np.dot(x.T, delta_error))
                 )
-                print(delta_weight)
                 new_layer[i] = self.neural_network.list_of_layer[i].get_updated_weight(
                     delta_weight
                 )
@@ -56,7 +55,6 @@ class MiniBatch(NamedTuple):
                 delta_weight = np.array(
                     np.dot(learning_rate, np.dot(x.T, delta_error))
                 )
-                print(delta_weight)
                 new_layer[i] = self.neural_network.list_of_layer[i].get_updated_weight(
                     delta_weight
                 )
